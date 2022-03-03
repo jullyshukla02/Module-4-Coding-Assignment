de60ca9508f854de96831d4588e1c6ccfea7074b
Jully Shukla <jullyshukla19@gmail.com>
2022-03-03 23:36:35 +0530

    Create SpeakHello.js

---
+++ SpeakHello.js
@@ -0,0 +1,20 @@
+(function(window) {
+	
+
+// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+
+var helloSpeaker = {}
+
+// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+var speakWord = "Hello";
+
+// STEP 4: Rewrite the 'speak' function such that it is attached to the
+
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
+
+window.helloSpeaker = helloSpeaker;
+})(window);
