4dde87990f9f158e1c9a724b168f1f5bb9ab691b
Jully Shukla <jullyshukla19@gmail.com>
2022-03-03 23:39:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+(function() {
+  
+
+// STEP 1:
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+// STEP 10:
+
+for (var i = 0; i < names.length; i++) {
+
+  // STEP 11:
+ 
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  // STEP 12:
+ 
+  if (firstLetter == "j") {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
